5b3a335b448f2df2a6f6c91a2ccc2c1f92cdf62d
petit_x <petit_x@epitech.eu>
2016-01-18 11:19:29 +0100

    03

---
+++ ex03/Buzz.h
@@ -0,0 +1,27 @@
+/*
+** Buzz.h for Buzz/home/petit_x/Epitech/rendu/piscine_cpp/piscine_cpp_d13/ex00/Buzz.h
+** 
+** Made by  petit_x - Marlon Petit
+** Login   < petit_x@epitech.net >
+** 
+** Started on  Mon Jan 18 10:23:38 2016 petit_x - Marlon Petit
+** Last update Mon Jan 18 11:18:19 2016 Marlon Petit
+*/
+
+#ifndef BUZZ_H_
+# define BUZZ_H_
+
+#include <iostream>
+#include <string>
+
+#include "Toy.h"
+
+class		Buzz : public Toy
+{
+public:
+  Buzz(std::string, const std::string&);
+  Buzz(std::string);
+  void		speak(std::string);
+};
+
+#endif /* !BUZZ_H_ */
---
+++ ex03/Toy.cpp
@@ -0,0 +1,65 @@
+/*
+** Toy.cpp for Toy/home/petit_x/Epitech/rendu/piscine_cpp/piscine_cpp_d13/ex00/Toy.cpp
+** 
+** Made by  petit_x - Marlon Petit
+** Login   < petit_x@epitech.net >
+** 
+** Started on  Mon Jan 18 10:23:45 2016 petit_x - Marlon Petit
+** Last update Mon Jan 18 11:14:06 2016 Marlon Petit
+*/
+
+#include "Toy.h"
+
+Toy::Toy(int type, std::string name, const std::string& file)
+{
+  this->type = type;
+  this->name = name;
+  this->pict.getPictureFromFile(file);
+}
+
+int	Toy::getType()
+{
+  return (this->type);
+}
+
+std::string	Toy::getName()
+{
+  return (this->name);
+}
+
+void	Toy::setName(std::string name)
+{
+  this->name = name;
+}
+
+Toy::Toy()
+{
+  this->type = BASIC_TOY;
+  this->name = "toy";
+}
+
+bool	Toy::setAscii(const std::string& file)
+{
+  return(this->pict.getPictureFromFile(file));
+}
+
+std::string	Toy::getAscii()
+{
+  return (this->pict.data);
+}
+
+Toy::~Toy()
+{}
+
+Toy&	Toy::operator=(Toy const& toy)
+{
+  this->type = toy.type;
+  this->name = toy.name;
+  this->pict = toy.pict;
+  return (*this);
+}
+
+void	Toy::speak(std::string sent)
+{
+  std::cout << this->name << " \"" << sent << "\"" << std::endl;
+}
